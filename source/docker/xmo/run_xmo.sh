4fa6dd58d6db2e7caec5a49f8bf5fef8805b1492
Harold Pulcher <pulcher@killercomputing.com>
2022-01-27 03:30:48 +0000

    got the volumes being mounted

--- source/docker/xmo/run_xmo.sh
+++ source/docker/xmo/run_xmo.sh
@@ -3,6 +3,10 @@
 docker run \
     --rm \
     -it \
+    -v ~/repos/Xmo/source/xmo_packages:/root/dev_ws/src \
+    -v ~/repos/Xmo/source/launch:/root/dev_ws/launch \
+    -v ~/repos/Xmo/source/config:/root/dev_ws/config \
+    -v ~/repos/Xmo/source/install:/root/dev_ws/install \
     --name xmo \
     -h xmo \
     --network="host" \

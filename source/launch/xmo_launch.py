2ad43f6df8cb3e2f9c0c06931ae2403cf295016a
Harold Pulcher <pulcher@killercomputing.com>
2022-02-03 04:09:58 +0000

    got the joy xlated to a cmd_vel

--- source/launch/xmo_launch.py
+++ source/launch/xmo_launch.py
@@ -7,5 +7,27 @@ def generate_launch_description():
             package='joy',
             executable='joy_node',
             name='joystick'
+        ),
+        Node(
+            package='teleop_twist_joy',
+            executable='teleop_node',
+            name='teleop_twist_joy_node',
+            parameters=[
+                {'enable_button': -1},
+                {'require_enable_button': False},
+                {'axis_linear.x': 0},
+                {'axis_linear.y': 1},
+                {'axis_linear.z': 1},
+                {'axis_angular.pitch': 3},
+                {'axis_angular.roll': 2},
+                {'axis_angular.yaw': 3},
+                {'scale_linear.x': -90.0},
+                {'scale_linear.y': 90.0},
+                {'scale_linear.z': 1.0},
+                {'scale_angular.pitch': 90.0},
+                {'scale_angular.roll': -90.0},
+                {'scale_angular.yaw': 1.0}
+            ],
+            arguments=['--ros-args', '--log-level', 'info']
         )
     ])

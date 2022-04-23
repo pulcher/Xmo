03e5d11850d3c9b9659e46b61ad3fa3a4ec50959
Harold Pulcher <pulcher@killercomputing.com>
2022-04-23 03:41:27 +0000

    got some math working on the ackerman

--- source/xmo_packages/xmo_py/xmo_py/ackerman_drive_node.py
+++ source/xmo_packages/xmo_py/xmo_py/ackerman_drive_node.py
@@ -1,4 +1,5 @@
 import asyncio
+import math
 from ast import Subscript
 import rclpy
 from rclpy.node import Node
@@ -38,18 +39,37 @@ class AckermanNode(Node):
                  10)
         self.subscription  # prevent unused variable warning
 
+        # set some physical values
+        self.wheel_center_to_drive_center = 160
+        self.wheel_center_width = 220
+
         # Set the min and max radius for the robot in millimeters
         self.radius_min = 110
         self.radius_max = 9166
 
 
     async def listener_callback(self, msg):
-        # left side
+
+        if msg.angular.x != 0.0:
+
+            denominator_2 = self.wheel_center_to_drive_center/math.tan(math.radians(msg.angular.x))
+
+            # generate the inside angle from the joystick angle
+            denominator = denominator_2 - self.wheel_center_width
+
+            inside_angle =  math.degrees(math.atan( self.wheel_center_to_drive_center/denominator ))
+
+            self.get_logger().info("angle: %f, tan: %f, denominator: %f, denominator_2: %f, inside_angle: %f" %
+                    (msg.angular.x, math.tan(msg.angular.x), denominator, denominator_2, inside_angle))
+
+        else:
+            inside_angle = msg.angular.x
 
         # front wheel steering angles
         await self.send_servo_msg(ServoNames.lf_steer_node, msg.angular.x)
+        await self.send_servo_msg(ServoNames.rf_steer_node, inside_angle)
+
         await self.send_servo_msg(ServoNames.lf_drive_node, -msg.angular.y)
-        await self.send_servo_msg(ServoNames.rf_steer_node, msg.angular.x)
         await self.send_servo_msg(ServoNames.rf_drive_node, msg.angular.y)
 
         # no change for the middle steering angles
